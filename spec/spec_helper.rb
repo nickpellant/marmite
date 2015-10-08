9841294a85aeba9dd815b4e00e8f8eddde3f4c4f
Nick Pellant <nick@nickpellant.com>
2015-10-08 22:42:00 +0100

    Setup CodeClimate coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
-SimpleCov.start
+require 'codeclimate-test-reporter'
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'marmite'
